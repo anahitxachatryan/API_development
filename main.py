a21357b9884055171bf04107213b4250b2f739fb
anahit <anahit@noblescripts.com>
2021-12-17 16:54:13 +0400

    added post method -create_post

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.param_functions import Body
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title:str
+    content:str
+    public:bool = True
+    voting:Optional[int] = None
+    
+
+
 
 @app.get("/")
 def root():
@@ -9,4 +22,10 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"message":"Posts are located here!"}
+
+
+@app.post("/createposts")
+def create_post(post: Post):
+    print(post.title)
+    return {"data":post}
 
