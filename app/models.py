c142b86d72b926b965014a1395dbd63fbe8c8d5b
anahit <xachatryan96an@gmail.com>
2021-12-26 22:34:58 +0400

    minor changes

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from .database import Base
 from sqlalchemy import Column, String
 
 class Post(Base):
+    
     __tablename__ = "posts"
     id = Column(Integer, primary_key= True, nullable= False)
     title  = Column(String, nullable=False)
